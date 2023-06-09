1f9ea8abbc2778bfc228d480ac53487c1216cd9d
FatimaMammadova95 <6lzpze1@code.edu.az>
2023-06-10 01:36:53 +0400

    added counter js

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -28,6 +28,28 @@ function scrollFunction() {
 }
 window.addEventListener("scroll", scrollFunction);
 
+// Counter
+let a = 0;
+$(window).scroll(function () {
+  let oTop = $("#counter").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $(".count").each(function () {
+      let $this = $(this);
+      jQuery({ Counter: 0 }).animate(
+        { Counter: $this.text() },
+        {
+          duration: 2000,
+          easing: "swing",
+          step: function () {
+            $this.text(Math.ceil(this.Counter));
+          },
+        }
+      );
+    });
+    a = 1;
+  }
+});
+
 // Swipper
 var swiper = new Swiper("#hero-swiper", {
   spaceBetween: 30,
