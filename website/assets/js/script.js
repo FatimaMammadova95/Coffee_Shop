5ef6a5d8139717ea9444c9a07defabcc47f5a21f
FatimaMammadova95 <6lzpze1@code.edu.az>
2023-06-13 18:42:53 +0400

    extract some scripts to another file

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -1,33 +1,3 @@
-const menuIcon = document.querySelector("#menu");
-const burgerMenu = document.querySelector("#burger-menu");
-const header = document.querySelector("header");
-
-// Menu click
-
-menu.addEventListener("click", function (e) {
-  if (this.classList.contains("fa-bars")) {
-    this.classList = "fa-solid fa-xmark";
-    burgerMenu.style.display = "flex";
-  } else {
-    this.classList = "fa-solid fa-bars";
-    burgerMenu.style.display = "none";
-  }
-});
-
-// Scroll nav
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    header.style.background = "rgba(0, 0, 0, 0.451)";
-  } else {
-    header.style.background = "";
-  }
-}
-window.addEventListener("scroll", scrollFunction);
-
 // Counter
 let a = 0;
 $(window).scroll(function () {
