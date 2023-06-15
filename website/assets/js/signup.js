42d1168b99e2db2feda9a8580272073cdd19a639
FatimaMammadova95 <6lzpze1@code.edu.az>
2023-06-15 21:22:52 +0400

    added sign up functionality

--- website/assets/js/signup.js
+++ website/assets/js/signup.js
@@ -1,7 +1,8 @@
-let username = document.querySelector(".username");
-let email = document.querySelector(".email");
-let password = document.querySelector(".password");
-let confrimPassword = document.querySelector(".confrim-password");
+const BASE_URL = "http://localhost:8080/";
+
+let username = document.querySelector("#username");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
 let forms = document.querySelectorAll(".needs-validation");
 
 Array.prototype.slice.call(forms).forEach(function (form) {
@@ -11,6 +12,14 @@ Array.prototype.slice.call(forms).forEach(function (form) {
       if (!form.checkValidity()) {
         event.preventDefault();
         event.stopPropagation();
+  
+      }else{
+        let user = {
+            username: username.value,
+            email: email.value,
+            password: password.value,
+          };
+          axios.post(`${BASE_URL}users`, user);
       }
 
       form.classList.add("was-validated");
