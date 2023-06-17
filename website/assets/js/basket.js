6fb878edb7bc9d6a6ee7735e554b176454d099c3
FatimaMammadova95 <6lzpze1@code.edu.az>
2023-06-17 04:51:03 +0400

    added basket responsibility

--- website/assets/js/basket.js
+++ website/assets/js/basket.js
@@ -1,42 +1,71 @@
-(function () {
-  window.inputNumber = function (el) {
-    let min = el.attr("min") || false;
-    let max = el.attr("max") || false;
+let row = document.querySelector(".card-row");
+let total = document.querySelector(".total");
+let cargo = document.querySelector(".cargo");
+let basket = JSON.parse(localStorage.getItem("basket"));
 
-    let els = {};
+window.inputNumber = function (el) {
+  let min = el.attr("min") || false;
+  let max = el.attr("max") || false;
 
-    els.dec = el.prev();
-    els.inc = el.next();
+  let els = {};
 
-    el.each(function () {
-      init($(this));
-    });
+  els.dec = el.prev();
+  els.inc = el.next();
 
-    function init(el) {
-      els.dec.on("click", decrement);
-      els.inc.on("click", increment);
-      console.log(el[0].value);
+  el.each(function () {
+    init($(this));
+  });
 
-      function decrement() {
-        let value = el[0].value;
-        value--;
-        if (!min || value >= min) {
-          el[0].value = value;
-        }
-      }
+  function init(el) {
+    els.dec.on("click", decrement);
+    els.inc.on("click", increment);
 
-      function increment() {
-        let value = el[0].value;
-        value++;
-        if (!max || value <= max) {
-          el[0].value = value++;
-        }
+    function decrement() {
+      let value = el[0].value;
+      value--;
+      if (!min || value >= min) {
+        el[0].value = value;
       }
     }
-  };
-})();
 
-inputNumber($(".input-number"));
+    function increment() {
+      let value = el[0].value;
+      value++;
+      if (!max || value <= max) {
+        el[0].value = value++;
+      }
+    }
+  }
+};
 
-// let number = document.querySelector(".input-number")
-// console.log(number.value);
+function getData() {
+  basket.forEach((element) => {
+    row.innerHTML += `
+    <div class="col-12">
+    <div class="product-about">
+      <img src=${element.image} alt="" />
+      <div class="product-text">
+        <h1>${element.name}</h1>
+        <p>${element.description}</p>
+      </div>
+    </div>
+    <div class="value">
+      <div class="number">
+        <span class="input-number-decrement">-</span
+        ><input
+          class="input-number"
+          type="text"
+          value="1"
+          min="1"
+          max="10"
+        /><span class="input-number-increment">+</span>
+      </div>
+      <p class="price-card">${element.price}$</p>
+      <i class="fa-solid fa-trash"></i>
+    </div>
+  </div>
+    `;
+    inputNumber($(".input-number"));
+  });
+}
+getData();
