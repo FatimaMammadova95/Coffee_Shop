f08af741d18e2abc5dfd4a7bcc2ebf37ac77c6b0
FatimaMammadova95 <6lzpze1@code.edu.az>
2023-06-18 20:37:31 +0400

    added reservation page

---
+++ admin/assets/js/reservation.js
@@ -0,0 +1,38 @@
+const BASE_URL = "http://localhost:8080/";
+
+const tBody = document.querySelector("tbody");
+const searchInput = document.querySelector("#simple-search");
+
+let dataArr = [];
+let copyArr = [];
+let sortedArr = [];
+
+function createCard(arr) {
+  tBody.innerHTML = "";
+  arr.forEach((element, index) => {
+    tBody.innerHTML += `
+<tr>
+<td>${index}</td>
+<td>${element.numberOfCustomer}</td>
+<td class="description">${element.selectTime}</td>
+</tr>`;
+  });
+}
+
+async function getData() {
+  let res = await axios(`${BASE_URL}reservation`);
+  dataArr = res.data;
+  copyArr = searchInput.value || copyArr.length ? copyArr : res.data;
+
+  createCard(copyArr);
+}
+getData();
+
+// Search
+searchInput.addEventListener("input", function (e) {
+  copyArr = sortedArr.length ? sortedArr : dataArr;
+  copyArr = copyArr.filter((item) =>
+    item.name.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+  );
+  createCard(copyArr);
+});
