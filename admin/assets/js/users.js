9f22f08227d63db55d22fa0573c23f60c2c0f285
FatimaMammadova95 <6lzpze1@code.edu.az>
2023-06-18 20:32:53 +0400

    added users page

---
+++ admin/assets/js/users.js
@@ -0,0 +1,39 @@
+const BASE_URL = "http://localhost:8080/";
+
+const tBody = document.querySelector("tbody");
+const searchInput = document.querySelector("#simple-search");
+
+let dataArr = [];
+let copyArr = [];
+let sortedArr = [];
+
+function createCard(arr) {
+  tBody.innerHTML = "";
+  arr.forEach((element, index) => {
+    tBody.innerHTML += `
+<tr>
+<td>${index}</td>
+<td>${element.username}</td>
+<td class="description">${element.email}</td>
+<td>${element.password}</td>
+</tr>`;
+  });
+}
+
+async function getData() {
+  let res = await axios(`${BASE_URL}users`);
+  dataArr = res.data;
+  copyArr = searchInput.value || copyArr.length ? copyArr : res.data;
+
+  createCard(copyArr);
+}
+getData();
+
+// Search
+searchInput.addEventListener("input", function (e) {
+  copyArr = sortedArr.length ? sortedArr : dataArr;
+  copyArr = copyArr.filter((item) =>
+    item.name.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+  );
+  createCard(copyArr);
+});
